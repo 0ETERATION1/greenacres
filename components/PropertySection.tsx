f019c2bb7f4dc73e7ebd2889e4dc824f4a00c27c
Alex Terskin <terskinalex@gmail.com>
2025-01-10 16:08:36 -0500

    revert changes for mobile

--- components/PropertySection.tsx
+++ components/PropertySection.tsx
@@ -5,132 +5,201 @@ export default function PropertySection() {
   return (
     <section className="py-10">
       <div className="container mx-auto">
-        {/* First Section */}
-        <div className="flex flex-col md:items-center md:flex-row mb-10">
-          <div className="w-full md:w-1/2 px-5 md:px-0 md:pr-2 order-3 md:order-none">
+        {/* First Image and Text */}
+        <div className="flex items-center mb-10">
+          <div className="w-1/2 place-items-end px-5">
             <Image
               src="/assets/images/port/bradleyAndDad.JPG"
               alt="Dad mowing Lawn and leaving stripes in Montgomery County Maryland"
               width={300}
               height={400}
-              className="rounded-image custom-image mx-auto md:ml-auto"
+              className="rounded-image custom-image"
             />
-            <div className="flex justify-center mt-4 md:hidden">
-              <Link
-                href="/about"
-                className="inline-block px-4 py-2 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md transition-transform duration-300 ease-in-out hover:-translate-y-0.5 shadow-md whitespace-nowrap w-[140px] text-center"
-                style={{ fontWeight: 500 }}
-              >
-                Our story 📖
-              </Link>
-            </div>
           </div>
-          <div className="w-full md:w-1/2 pl-6 md:pl-4 order-1 md:order-none">
-            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4 text-center md:text-left">
+          <div className="w-1/2 pl-6">
+            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
               What we bring to the table
             </h1>
-            <p className="text-center md:text-left mb-6 md:mb-4">
+            <p>
               With over three decades of experience in Montgomery County, we
               bring expertise, dedication, and a family-oriented approach to
               every project. Our commitment to quality and attention to detail
               ensures your outdoor space receives the care it deserves.
             </p>
-            <div className="hidden md:block">
-              <Link
-                href="/about"
-                className="inline-block px-4 py-2 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md transition-transform duration-300 ease-in-out hover:-translate-y-0.5 shadow-md whitespace-nowrap w-[140px] text-center"
-                style={{ fontWeight: 500 }}
-              >
-                Our story 📖
-              </Link>
-            </div>
+            <br></br>
+            <Link
+              href="/about"
+              className="
+                inline-block px-4 py-2
+                bg-gradient-to-r from-green-600 to-green-700
+                text-white
+                rounded-md
+                transition-transform duration-300 ease-in-out
+                hover:-translate-y-0.5
+                shadow-md
+                whitespace-nowrap
+                w-[140px]
+                text-center
+                
+              "
+              style={{ fontWeight: 500 }}
+            >
+              Our story 📖
+              {/* <span>Free Quote!</span> <span className="text-lg">🌳</span> */}
+            </Link>
           </div>
         </div>
 
-        {/* Second Section */}
-        <div className="flex flex-col md:items-center md:flex-row mb-10">
-          <div className="w-full md:w-1/2 pl-6 md:pl-4 order-1 md:order-none">
-            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4 text-center md:text-right">
+        {/* Second Image and Text */}
+        <div className="flex items-center mb-10">
+          <div className="w-1/2 place-items-end px-5">
+            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
               Our Skillset
             </h1>
-            <p className="text-center md:text-right mb-6 md:mb-4">
+            <p className="text-left md:text-right">
               From precision mowing and seasonal maintenance to comprehensive
               lawn care, our Turf Program delivers year-round solutions that
               keep your property healthy, vibrant, and beautiful in every
               season.
             </p>
-            <div className="hidden md:block md:text-right">
-              <Link
-                href="/services"
-                className="inline-block px-4 py-2 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md transition-transform duration-300 ease-in-out hover:-translate-y-0.5 shadow-md whitespace-nowrap w-[140px] text-center"
-                style={{ fontWeight: 500 }}
-              >
-                Services 🛠️
-              </Link>
-            </div>
+            <br></br>
+            <Link
+              href="/services"
+              className="
+                inline-block px-4 py-2
+                bg-gradient-to-r from-green-600 to-green-700
+                text-white
+                rounded-md
+                transition-transform duration-300 ease-in-out
+                hover:-translate-y-0.5
+                shadow-md
+                whitespace-nowrap
+                w-[140px]
+                text-center
+              "
+              style={{ fontWeight: 500 }}
+            >
+              Services 🛠️
+            </Link>
           </div>
-          <div className="w-full md:w-1/2 px-5 md:px-0 md:pl-2 order-3 md:order-none">
+          <div className="w-1/2 place-items-start px-5">
             <Image
               src="/assets/images/port/dadMowing.jpg"
               alt="Description of the second image"
               width={300}
               height={400}
-              className="rounded-image custom-image mx-auto md:mr-auto"
+              className="rounded-image custom-image"
             />
-            <div className="flex justify-center mt-4 md:hidden">
-              <Link
-                href="/services"
-                className="inline-block px-4 py-2 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md transition-transform duration-300 ease-in-out hover:-translate-y-0.5 shadow-md whitespace-nowrap w-[140px] text-center"
-                style={{ fontWeight: 500 }}
-              >
-                Services 🛠️
-              </Link>
-            </div>
           </div>
         </div>
 
-        {/* Third Section */}
-        <div className="flex flex-col md:items-center md:flex-row">
-          <div className="w-full md:w-1/2 px-5 md:px-0 md:pr-2 order-3 md:order-none">
+        {/* Third Image and Text */}
+        <div className="flex items-center">
+          <div className="w-1/2 place-items-end px-5">
             <Image
               src="/assets/images/port/Houseblossom.jpg"
               alt="Description of the third image"
               width={300}
               height={400}
-              className="rounded-image custom-image mx-auto md:ml-auto"
+              className="rounded-image custom-image"
             />
-            <div className="flex justify-center mt-4 md:hidden">
-              <Link
-                href="/portfolio"
-                className="inline-block px-4 py-2 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md transition-transform duration-300 ease-in-out hover:-translate-y-0.5 shadow-md whitespace-nowrap w-[140px] text-center"
-                style={{ fontWeight: 500 }}
-              >
-                Portfolio 🏡
-              </Link>
-            </div>
           </div>
-          <div className="w-full md:w-1/2 pl-6 md:pl-4 order-1 md:order-none">
-            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4 text-center md:text-left">
+          <div className="w-1/2 pl-6">
+            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
               See your house blossom
             </h1>
-            <p className="text-center md:text-left mb-6 md:mb-4">
+            <p>
               Transform your outdoor space into a stunning landscape that
               enhances your property&apos;s beauty and value. Browse our
               portfolio to see how we&apos;ve helped countless homeowners create
               their dream gardens and landscapes.
             </p>
-            <div className="hidden md:block">
-              <Link
-                href="/portfolio"
-                className="inline-block px-4 py-2 bg-gradient-to-r from-green-600 to-green-700 text-white rounded-md transition-transform duration-300 ease-in-out hover:-translate-y-0.5 shadow-md whitespace-nowrap w-[140px] text-center"
-                style={{ fontWeight: 500 }}
-              >
-                Portfolio 🏡
-              </Link>
-            </div>
+            <br></br>
+            <Link
+              href="/portfolio"
+              className="
+                inline-block px-4 py-2
+                bg-gradient-to-r from-green-600 to-green-700
+                text-white
+                rounded-md
+                transition-transform duration-300 ease-in-out
+                hover:-translate-y-0.5
+                shadow-md
+                whitespace-nowrap
+                w-[140px]
+                text-center
+                
+              "
+              style={{ fontWeight: 500 }}
+            >
+              Portfolio 🏡
+              {/* <span>Free Quote!</span> <span className="text-lg">🌳</span> */}
+            </Link>
           </div>
         </div>
       </div>
     </section>
+    // <section className="property-section">
+    //   <div className="container">
+    //     <div className="section-content">
+    //       {/* Left side - Images */}
+    //       <div className="images-grid">
+    //         <div className="image-row">
+    //           <div className="image-wrapper small">
+    //             <Image
+    //               src="/assets/images/port/dadMowing.jpg"
+    //               alt="Dad mowing Lawn and leaving stripes in Montgomery County Maryland"
+    //               width={744}
+    //               height={368}
+    //               className="rounded-image"
+    //             />
+    //           </div>
+    //           <div className="image-wrapper medium">
+    //             <Image
+    //               src="/assets/images/port/powerwashing.jpg"
+    //               alt="Power washing concrete in Montgomery County Maryland"
+    //               width={400}
+    //               height={200}
+    //               className="rounded-image"
+    //             />
+    //           </div>
+    //         </div>
+    //         <div className="image-row">
+    //           <div className="image-wrapper large">
+    //             <Image
+    //               src="/assets/images/port/shrub_trimming.jpg"
+    //               alt="Shrub trimming in Montgomery County Maryland"
+    //               width={500}
+    //               height={300}
+    //               className="rounded-image"
+    //             />
+    //           </div>
+    //           <div className="image-wrapper small">
+    //             <Image
+    //               src="/assets/images/port/silverspringTurf.jpg"
+    //               alt="grass in Montgomery County Maryland"
+    //               width={300}
+    //               height={200}
+    //               className="rounded-image"
+    //             />
+    //           </div>
+    //         </div>
+    //       </div>
+
+    //       {/* Right side - Text */}
+    //       <div className="content-text">
+    //         <h2>Keep your Property looking its Best</h2>
+    //         <p>
+    //           With 30+ years experience we strive to consistently bring value to
+    //           our customers. Save time & hassle and hire us for all your outdoor
+    //           needs.
+    //         </p>
+    //         <Link href="/portfolio" className="portfolio-link">
+    //           View Portfolio
+    //         </Link>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
