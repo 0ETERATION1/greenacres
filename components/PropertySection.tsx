d3f7ec737bd900bfd4939c57da2c251724a2c227
Alex Terskin <terskinalex@gmail.com>
2025-12-25 10:16:20 -0600

    Home page - the text shouldnt outstretch the width of the images

--- components/PropertySection.tsx
+++ components/PropertySection.tsx
@@ -18,55 +18,19 @@ export default function PropertySection() {
             />
           </div>
           <div className={`w-1/2 pl-6 ${styles.textContainer}`}>
-            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
-              What we bring to the table
-            </h1>
-            <p>
-              With over three decades of experience in Montgomery County, we
-              bring expertise, dedication, and a family-oriented approach to
-              every project. Our commitment to quality and attention to detail
-              ensures your outdoor space receives the care it deserves.
-            </p>
-            <br></br>
-            <Link
-              href="/about"
-              className="
-                inline-block px-4 py-2
-                bg-gradient-to-r from-green-600 to-green-700
-                text-white
-                rounded-md
-                transition-transform duration-300 ease-in-out
-                hover:-translate-y-0.5
-                shadow-md
-                whitespace-nowrap
-                w-[140px]
-                text-center
-                
-              "
-              style={{ fontWeight: 500 }}
-            >
-              Our story 📖
-              {/* <span>Free Quote!</span> <span className="text-lg">🌳</span> */}
-            </Link>
-          </div>
-        </div>
-
-        {/* Second Image and Text */}
-        <div className="flex items-center mb-10">
-          <div className={`w-1/2 pr-6 ${styles.textContainer}`}>
-            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
-              Our Skillset
-            </h1>
-            <p>
-              From precision mowing and seasonal maintenance to comprehensive
-              lawn care, our Turf Program delivers year-round solutions that
-              keep your property healthy, vibrant, and beautiful in every
-              season.
-            </p>
-            <br></br>
-            <div className="flex justify-end">
+            <div className="max-w-[340px]">
+              <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
+                What we bring to the table
+              </h1>
+              <p>
+                With over three decades of experience in Montgomery County, we
+                bring expertise, dedication, and a family-oriented approach to
+                every project. Our commitment to quality and attention to detail
+                ensures your outdoor space receives the care it deserves.
+              </p>
+              <br></br>
               <Link
-                href="/services"
+                href="/about"
                 className="
                   inline-block px-4 py-2
                   bg-gradient-to-r from-green-600 to-green-700
@@ -82,8 +46,50 @@ export default function PropertySection() {
                 "
                 style={{ fontWeight: 500 }}
               >
-                Services 🛠️
+                Our story 📖
+                {/* <span>Free Quote!</span> <span className="text-lg">🌳</span> */}
               </Link>
+            </div>
+          </div>
+        </div>
+
+        {/* Second Image and Text */}
+        <div className="flex items-center mb-10">
+          <div
+            className={`w-1/2 pr-6 flex justify-end ${styles.textContainer}`}
+          >
+            <div className="max-w-[340px]">
+              <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
+                Our Skillset
+              </h1>
+              <p>
+                From precision mowing and seasonal maintenance to comprehensive
+                lawn care, our Turf Program delivers year-round solutions that
+                keep your property healthy, vibrant, and beautiful in every
+                season.
+              </p>
+              <br></br>
+              <div className="flex justify-end">
+                <Link
+                  href="/services"
+                  className="
+                  inline-block px-4 py-2
+                  bg-gradient-to-r from-green-600 to-green-700
+                  text-white
+                  rounded-md
+                  transition-transform duration-300 ease-in-out
+                  hover:-translate-y-0.5
+                  shadow-md
+                  whitespace-nowrap
+                  w-[140px]
+                  text-center
+                  
+                "
+                  style={{ fontWeight: 500 }}
+                >
+                  Services 🛠️
+                </Link>
+              </div>
             </div>
           </div>
           <div
@@ -109,36 +115,38 @@ export default function PropertySection() {
             />
           </div>
           <div className={`w-1/2 pl-6 ${styles.textContainer}`}>
-            <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
-              See your house blossom
-            </h1>
-            <p>
-              Transform your outdoor space into a stunning landscape that
-              enhances your property&apos;s beauty and value. Browse our
-              portfolio to see how we&apos;ve helped countless homeowners create
-              their dream gardens and landscapes.
-            </p>
-            <br></br>
-            <Link
-              href="/gallery"
-              className="
-                inline-block px-4 py-2
-                bg-gradient-to-r from-green-600 to-green-700
-                text-white
-                rounded-md
-                transition-transform duration-300 ease-in-out
-                hover:-translate-y-0.5
-                shadow-md
-                whitespace-nowrap
-                w-[140px]
-                text-center
-                
-              "
-              style={{ fontWeight: 500 }}
-            >
-              Gallery 🖼️
-              {/* <span>Free Quote!</span> <span className="text-lg">🌳</span> */}
-            </Link>
+            <div className="max-w-[340px]">
+              <h1 className="text-3xl md:text-4xl font-bold text-[#0cabba] mb-4">
+                See your house blossom
+              </h1>
+              <p>
+                Transform your outdoor space into a stunning landscape that
+                enhances your property&apos;s beauty and value. Browse our
+                portfolio to see how we&apos;ve helped countless homeowners
+                create their dream gardens and landscapes.
+              </p>
+              <br></br>
+              <Link
+                href="/gallery"
+                className="
+                  inline-block px-4 py-2
+                  bg-gradient-to-r from-green-600 to-green-700
+                  text-white
+                  rounded-md
+                  transition-transform duration-300 ease-in-out
+                  hover:-translate-y-0.5
+                  shadow-md
+                  whitespace-nowrap
+                  w-[140px]
+                  text-center
+                  
+                "
+                style={{ fontWeight: 500 }}
+              >
+                Gallery 🖼️
+                {/* <span>Free Quote!</span> <span className="text-lg">🌳</span> */}
+              </Link>
+            </div>
           </div>
         </div>
       </div>
