25cd2abd4a050e6003ede3975ec5b5905c823991
Alex Terskin <terskinalex@gmail.com>
2025-12-25 10:38:15 -0600

    green footer stretching the whole page

--- components/FirewoodSection.tsx
+++ components/FirewoodSection.tsx
@@ -3,20 +3,18 @@ import Link from "next/link";
 export default function FirewoodSection() {
   return (
     <section className="firewood-section">
-      <div className="container">
-        <div className="firewood-banner" id="firewoodAD">
-          <div className="banner-content">
-            <h3>Get seasoned hardwood firewood delivered to your door!</h3>
-            <p>
-              We deliver a 1/4 of a cord and up. Smaller quantities are
-              available for pickup!
-            </p>
-            <br></br>
-            <div className="">
-              <Link href="/firewood" className="fire-order-button">
-                Order now! 🔥🪵
-              </Link>
-            </div>
+      <div className="firewood-banner" id="firewoodAD">
+        <div className="banner-content">
+          <h3>Get seasoned hardwood firewood delivered to your door!</h3>
+          <p>
+            We deliver a 1/4 of a cord and up. Smaller quantities are available
+            for pickup!
+          </p>
+          <br></br>
+          <div className="">
+            <Link href="/firewood" className="fire-order-button">
+              Order now! 🔥🪵
+            </Link>
           </div>
         </div>
       </div>
